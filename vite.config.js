267abb0147eb0e497154f0a710ec8a4172b2f3f5
syf <p-shiyanfeng@xiaomi.com>
2026-04-03 13:16:16 +0800

    111

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/door-app/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/' : './',
   resolve: {
     alias: {
       react: path.resolve(__dirname, 'node_modules/react'),
