cfa0203c68d070ebcc208517742c977df089ee1e
syf <p-shiyanfeng@xiaomi.com>
2026-04-07 10:46:09 +0800

    111222

--- src/main.jsx
+++ src/main.jsx
@@ -5,20 +5,26 @@ import './index.css'
 
 // OneSignal 初始化
 window.OneSignal = window.OneSignal || [];
-OneSignal.push(function() {
-  // 直接修改默认值
-  window.OneSignal.SERVICE_WORKER_PATH = '/door-app/OneSignalSDKWorker.js';
+// 直接修改默认值，确保服务工作器路径正确
+if (window.OneSignal) {
+  window.OneSignal.SERVICE_WORKER_PATH = 'OneSignalSDKWorker.js';
   window.OneSignal.SERVICE_WORKER_PARAM = { scope: '/door-app/' };
-  
+}
+
+OneSignal.push(function() {
   OneSignal.init({
-    appId: "8c37e53b-1be9-422f-8efc-a2440a4e51af",
+    appId: "8c37e53b-1be9-422f-8efc-a2440a4e51af", // 替换为你的 OneSignal App ID
     autoRegister: true,
     notifyButton: {
       enable: true
     },
-    safari_web_id: "web.onesignal.auto.466e18d3-e8b5-4577-916c-b7c634612f08",
+    safari_web_id: "web.onesignal.auto.466e18d3-e8b5-4577-916c-b7c634612f08", // Safari 配置
     welcomeNotification: {
       disable: true
+    },
+    path: '/door-app/',
+    serviceWorkerParam: {
+      scope: '/door-app/'
     }
   });
 
